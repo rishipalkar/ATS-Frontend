f92d2f9525372777c9a271b32150753b247639ae
rishipalkar <hrishikeshpalkar88@gmail.com>
2026-03-17 18:41:13 +0530

    Ini Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { CandidateFormPageComponent } from './features/candidate-application/pages/candidate-form-page/candidate-form-page.component';
+import { RecruiterViewCandidateComponent } from './features/recruiter-view-candidates/pages/recruiter-view-candidate/recruiter-view-candidate.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'candidate/apply', pathMatch: 'full' },
+  { path: 'candidate/apply', component: CandidateFormPageComponent },
+  {path : '',redirectTo : 'recruiter/view-candidate',pathMatch:'full'},
+  {path : 'recruiter/view-candidate',component : RecruiterViewCandidateComponent},
+  {path : 'recruiter',component : RecruiterViewCandidateComponent},
+  {path : 'candidate',component : CandidateFormPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './core/app.component';
+import { CandidateFormPageComponent } from './features/candidate-application/pages/candidate-form-page/candidate-form-page.component';
+import { PersonalDetailsSectionComponent } from './features/candidate-application/components/personal-details-section/personal-details-section.component';
+import { EmploymentScreeningSectionComponent } from './features/candidate-application/components/employment-screening-section/employment-screening-section.component';
+import { ResumeUploadSectionComponent } from './features/candidate-application/components/resume-upload-section/resume-upload-section.component';
+import { ReferralSectionComponent } from './features/candidate-application/components/referral-section/referral-section.component';
+import { DiversitySectionComponent } from './features/candidate-application/components/diversity-section/diversity-section.component';
+import { PageHeaderComponent } from './core/shared/components/page-header/page-header.component';
+
+import { RecruiterViewCandidateComponent } from './features/recruiter-view-candidates/pages/recruiter-view-candidate/recruiter-view-candidate.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CandidateFormPageComponent,
+    PersonalDetailsSectionComponent,
+    EmploymentScreeningSectionComponent,
+    ResumeUploadSectionComponent,
+    ReferralSectionComponent,
+    DiversitySectionComponent,
+    PageHeaderComponent, 
+    RecruiterViewCandidateComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule,FormsModule,RouterModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
